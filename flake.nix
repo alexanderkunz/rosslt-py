cb9d60dee7772e26013ebe0adde28e16575ebded
Alexander Kunz <alexanderkunz@nachteule.org>
2025-01-07 06:30:16 +0100

    improve flake

--- flake.nix
+++ flake.nix
@@ -3,26 +3,37 @@
     nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
     nixpkgs.follows = "nix-ros-overlay/nixpkgs";
   };
-  outputs = { self, nix-ros-overlay, nixpkgs }:
-    nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nix-ros-overlay,
+      nixpkgs,
+    }:
+    nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ nix-ros-overlay.overlays.default ];
         };
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
-          name = "Example project";
+          name = "rosslt-py";
           packages = [
             pkgs.colcon
-            (with pkgs.rosPackages.humble; buildEnv {
-              paths = [
-                ros-core
-              ];
-            })
+            (
+              with pkgs.rosPackages.humble;
+              buildEnv {
+                paths = [
+                  ros-core
+                ];
+              }
+            )
           ];
         };
-      });
+      }
+    );
   nixConfig = {
     extra-substituters = [ "https://ros.cachix.org" ];
     extra-trusted-public-keys = [ "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo=" ];
